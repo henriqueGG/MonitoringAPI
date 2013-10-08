ca503aadd968f2a69e7ded1de023c01fbdacf061
tiago.nunes <tiago.nunes@cision.com>
2013-10-08 14:23:02 +0100

    inicio

---
+++ monitoring/monitoring.svc.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace monitoring
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    public class monitoring : Imonitoring
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        public string GetSampleMethod(string fullText, string since)
+        {
+            StringBuilder strReturnValue = new StringBuilder();
+            // return username prefixed as shown below
+            strReturnValue.Append(string.Format
+            ("{1}You have entered userName as {0}", fullText, since));
+            return strReturnValue.ToString();
+        }
+    }
+}
